ebb58c347438ee968389f48679940facb72851b5
Sam Gibson <sam@ifdown.net>
2013-03-14 23:48:06 +1100

    Automate testing with nestest

---
+++ cpu_test.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'rainbow'
+
+expected = File.new("assets/nestest.log").readlines.map { |l| l.gsub("\r\n", '')[0..72] }
+actual = `go run src/nestest.go`.split /\n/
+
+actual.zip(expected).each_with_index do |(a, e), i|
+  if e != a
+    if e.nil?
+      puts "!!!! #{a}"
+    end
+
+    expected_diff = e.chars.zip(a.chars).map { |ec, ac|
+      if ac != ec
+        ec.foreground(:green)
+      else
+        ec
+      end
+    }.join ''
+
+    actual_diff = a.chars.zip(e.chars).map { |ac, ec|
+      if ac != ec
+        ac.foreground(:red)
+      else
+        ac
+      end
+    }.join ''
+
+    if ![
+      5013, 5143, 5175, 5176, 5177, 5178, 5179, 5180, # Problems with $A9A9 -- is this a device?
+      8981, 8983, 8985, 8987, 8989 # Problems with $40xx, this is the APU, so no wonder there
+    ].include?(i+1)
+      ([0, i-15].max..i-1).each { |i| puts "                " + actual[i] }
+
+      puts sprintf("%-5d expected: %s", i + 1, expected_diff)
+      puts sprintf("%-5d actual  : %s", i + 1, actual_diff)
+      puts "\r\n"
+
+      break
+    end
+  end
+end
+
---
+++ src/cpu/cpu_unofficial_opcodes_test.go
@@ -0,0 +1,123 @@
+package cpu
+
+import "testing"
+
+func dcp(value byte) (*CPU) {
+    var p *CPU = new(CPU)
+
+    p.Memory[0] = value
+    p.Dcp(0)
+
+    return p
+}
+
+func lax(value byte) (*CPU) {
+    var p *CPU = new(CPU)
+
+    p.A, p.X = 0, 0
+    p.Memory[0] = value
+    p.Lax(0)
+
+    return p
+}
+
+func aac(first byte, second byte) (*CPU) {
+    var p *CPU = new(CPU)
+
+    p.A = first
+    p.Memory[0] = second
+    p.Aac(0)
+
+    return p
+}
+
+func sax(a byte, x byte) (*CPU) {
+    var p *CPU = new(CPU)
+
+    p.A = a
+    p.X = x
+    p.Sax(0)
+
+    return p
+}
+
+func TestAacSetsAccummulator(t *testing.T) {
+    p := aac(0xff, 0x44)
+
+    if p.A != 0x44 {
+        t.Errorf("AAC didn't and with the accumulator")
+        t.Errorf("Expected %#02x, got %#02x", 0x44, p.A)
+    }
+}
+
+func TestAacSetsCarryFlagWhenResultIsNegative(t *testing.T) {
+    p := aac(0xff, 0xff)
+
+    if !p.Carry() {
+        t.Errorf("Carry flag not set on negative result")
+    }
+}
+
+func TestAacSetsZeroFlagWhenResultIsZero(t *testing.T) {
+    p := aac(0x00, 0x00)
+
+    if !p.Zero() {
+        t.Errorf("Zero flag not set on zero result")
+    }
+}
+
+func TestLaxSetsAAndX(t *testing.T) {
+    p := lax(0xbb)
+
+    if p.X != 0xbb || p.A != 0xbb {
+        t.Errorf("Lax didn't load both A and X registers")
+    }
+}
+
+func TestLaxSetsZeroFlag(t *testing.T) {
+    p := lax(0x00)
+
+    if !p.Zero() {
+        t.Errorf("Lax didn't set the zero flag on zero result")
+    }
+}
+
+func TestLaxSetsNegativeFlag(t *testing.T) {
+    p := lax(0x80)
+
+    if !p.Negative() {
+        t.Errorf("Lax didn't set the negative flag on zero result")
+    }
+}
+
+func TestSaxAndsAAndX(t *testing.T) {
+    p := sax(0xff, 0x45)
+
+    if p.Memory[0] != 0x45 {
+        t.Errorf("Sax didn't and X and A into memory")
+    }
+}
+
+func TestSaxDoesntSetZeroFlag(t *testing.T) {
+    p := sax(0xff, 0x00)
+
+    if p.Zero() {
+        t.Errorf("Sax set the zero flag on zero result")
+    }
+}
+
+func TestSaxDoesntSetNegativeFlag(t *testing.T) {
+    p := sax(0x80, 0x80)
+
+    if p.Negative() {
+        t.Errorf("Sax set the negative flag on zero result")
+    }
+}
+
+func TstDcpSubs1FromMemory(t *testing.T) {
+    p := dcp(0x02)
+
+    if p.Memory[0] != 0x01 {
+        t.Errorf("Dcp didn't decrement memory")
+    }
+}
